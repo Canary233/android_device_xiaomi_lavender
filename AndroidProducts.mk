ff198fc316ca7ad3b15be8dfbf05f9743b802ac9
Canary233 <2096276316@qq.com>
2021-07-15 22:10:01 +0800

    exthm-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/exthm_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    exthm_lavender-user \
+    exthm_lavender-userdebug \
+    exthm_lavender-eng
